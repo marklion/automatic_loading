8b6ec85cd82e4470d51c62c77ad9694c46b00a61
marklion <marklion@sina.com>
2026-04-13 13:27:08 +0800

    ATDP-32 add pump control logic

--- src/modbus_io/modbus_io_idl.thrift
+++ src/modbus_io/modbus_io_idl.thrift
@@ -15,6 +15,12 @@ struct modbus_tcp_config{
     3: i32 device_id,
 }
 
+struct pump_param{
+    1: string open_device_name,
+    2: string close_device_name,
+    3: i32 delay_time,
+}
+
 service modbus_io_service{
     bool add_device(1:string device_name, 2:i32 channel_id, 3:bool is_output) throws (1: ad_modbus_io_gen_exp msg),
     void del_device(1:string device_name) throws (1: ad_modbus_io_gen_exp msg),
@@ -25,4 +31,7 @@ service modbus_io_service{
     modbus_tcp_config get_modbus_tcp() throws (1: ad_modbus_io_gen_exp msg),
     bool active_switch(1:bool turn_on) throws (1: ad_modbus_io_gen_exp msg),
     bool is_active() throws (1: ad_modbus_io_gen_exp msg),
+    void set_pump_param(1:pump_param param) throws (1: ad_modbus_io_gen_exp msg),
+    pump_param get_pump_param() throws (1: ad_modbus_io_gen_exp msg),
+    oneway void pump_control(1:bool turn_on),
 }
