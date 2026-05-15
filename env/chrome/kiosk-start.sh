d4fe35421c5e881d9db257fccebd9308906a44df
marklion <marklion@sina.com>
2026-05-15 11:14:26 +0800

    hotfix

--- env/chrome/kiosk-start.sh
+++ env/chrome/kiosk-start.sh
@@ -3,5 +3,4 @@
 sleep 2
 
 # 启动Chrome信息亭模式
-#microsoft-edge --kiosk http://localhost/#/video_cast --edge-kiosk-type=fullscreen
-google-chrome --kiosk http://localhost/#/video_cast
+google-chrome --check-for-update-interval=31536000 --disable-background-networking --noerrdialogs --kiosk http://localhost/#/video_cast
